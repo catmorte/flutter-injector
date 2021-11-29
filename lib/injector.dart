9629a5b5bc3b339b30769b23a24381e0e3f37e80
Raman_Susla <catmorte@gmail.com>
2021-11-29 22:06:00 +0300

    init

---
+++ lib/injector.dart
@@ -0,0 +1,86 @@
+typedef T InstanceFactory<T extends dynamic>(Getter getter);
+typedef T InstanceFactoryWithParams<T extends dynamic>(Getter getter, Map<String, dynamic> params);
+
+abstract class InstanceWrapper<T extends dynamic> {
+  InstanceFactoryWithParams<T> _factory;
+
+  InstanceWrapper(InstanceFactoryWithParams<T> value) : _factory = value;
+
+  T getValue(Getter getter, String key, {Map<String, dynamic> params = const {}}) => _factory(getter, params);
+}
+
+class Singleton<T extends dynamic> extends InstanceWrapper<T> {
+  T? _value;
+  bool _initialized = false;
+
+  Singleton(InstanceFactoryWithParams<T> value) : super(value);
+
+  @override
+  T getValue(Getter getter, String key, {Map<String, dynamic> params = const {}}) {
+    if (_initialized) {
+      return _value!;
+    }
+    _value = super.getValue(getter, key, params: params);
+    _initialized = true;
+    return _value!;
+  }
+}
+
+class Prototype<T extends dynamic> extends InstanceWrapper<T> {
+  Prototype(InstanceFactoryWithParams<T> value) : super(value);
+}
+
+abstract class Getter {
+  T get<T extends dynamic>({String key = "", Map<String, dynamic> params = const {}, Injector? childInjector});
+}
+
+class Injector implements Getter {
+  static Injector _root = Injector._();
+  static Map<String, Injector> _injectorsRegistry = {};
+
+  Injector? _parentInjector;
+  Map<String, InstanceWrapper> instanceWrappers = {};
+
+  Injector._([Injector? parentInjector]) : _parentInjector = parentInjector;
+
+  factory Injector.root() => _root;
+
+  Injector child() => Injector._(this);
+
+  Injector namedChild(String key, {clear: false}) {
+    if (clear) {
+      Injector newInstance = Injector._(this);
+      _injectorsRegistry[key] = newInstance;
+      return newInstance;
+    }
+    return _injectorsRegistry.putIfAbsent(key, () => Injector._(this));
+  }
+
+  _generateKey<T>(String key) {
+    String typeKey = T.toString();
+    return '$typeKey:$key';
+  }
+
+  T get<T extends dynamic>({String key = "", Map<String, dynamic> params = const {}, Injector? childInjector}) {
+    String genKey = _generateKey<T>(key);
+    InstanceWrapper? instanceWrapper = instanceWrappers[genKey];
+    if (instanceWrapper == null) {
+      if (_parentInjector != null) {
+        return _parentInjector!.get<T>(key: key, params: params, childInjector: childInjector ?? this);
+      }
+    }
+    assert(instanceWrapper != null, '$genKey is not initialized yet');
+    return instanceWrapper!.getValue(childInjector ?? this, genKey, params: params) as T;
+  }
+
+  void map<T extends dynamic>(InstanceFactory instanceFactory, {isSingleton = true, String key = ""}) {
+    String genKey = _generateKey<T>(key);
+    InstanceFactoryWithParams<T> factory = (i, _) => instanceFactory(i);
+    this.instanceWrappers[genKey] = isSingleton ? new Singleton(factory) : new Prototype(factory);
+  }
+
+  void mapWithParams<T extends dynamic>(InstanceFactoryWithParams instanceFactory, {String key = ""}) {
+    String genKey = _generateKey<T>(key);
+    this.instanceWrappers[genKey] = new Prototype(instanceFactory);
+  }
+}
---
+++ test/injector_test.dart
@@ -0,0 +1,124 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../lib/common/helpers/injector.dart';
+
+abstract class IX {
+  int getValue();
+}
+
+abstract class IY {
+  int getValue();
+
+  int getXValue();
+}
+
+abstract class IZ {}
+
+class X implements IX {
+  final int _value;
+
+  X(int value) : _value = value;
+
+  int getValue() => _value;
+}
+
+class Y implements IY {
+  final int _value;
+  final IX _x;
+
+  Y(IX x, int value)
+      : _x = x,
+        _value = value;
+
+  int getValue() => _value;
+
+  int getXValue() => _x.getValue();
+}
+
+void main() {
+  /* simple */
+  test('root', () {
+    final Injector ir = Injector.root();
+    int x = 0;
+    int y = 0;
+    ir.map<IX>((i) => X(++x));
+    ir.map<IY>((i) => Y(i.get<IX>(), ++y), isSingleton: false);
+    expect(ir.get<IX>().getValue(), 1);
+    expect(ir.get<IY>().getXValue(), 1);
+    expect(ir.get<IY>().getValue(), 2);
+    expect(ir.get<IX>().getValue(), 1);
+    expect(() => ir.get<IZ>(), throwsAssertionError);
+  });
+
+  test('child', () {
+    final Injector ic = Injector.root().child();
+    int x = 0;
+    int y = 0;
+    ic.map<IX>((i) => X(++x));
+    ic.map<IY>((i) => Y(i.get<IX>(), ++y), isSingleton: false);
+    expect(ic.get<IX>().getValue(), 1);
+    expect(ic.get<IY>().getXValue(), 1);
+    expect(ic.get<IY>().getValue(), 2);
+    expect(ic.get<IX>().getValue(), 1);
+    expect(() => ic.get<IZ>(), throwsAssertionError);
+  });
+
+  test('root & child', () {
+    final Injector ir = Injector.root();
+    ir.map<IX>((i) => X(100));
+    ir.map<IY>((i) => Y(i.get<IX>(), 100), isSingleton: false);
+    final Injector ic = Injector.root().child();
+    ic.map<IX>((i) => X(200));
+    expect(ic.get<IX>().getValue(), 200);
+    expect(ic.get<IY>().getXValue(), 200);
+    expect(ic.get<IY>().getValue(), 100);
+    expect(ic.get<IX>().getValue(), 200);
+    expect(ir.get<IX>().getValue(), 100);
+  });
+
+  /* with params */
+  test('root with params', () {
+    final Injector ir = Injector.root();
+    int x = 0;
+    int y = 0;
+    ir.map<IX>((i) => X(++x), key: "key");
+    ir.map<IY>((i) => Y(i.get<IX>(key: "key"), ++y), isSingleton: false, key: "key");
+    expect(ir.get<IX>(key: "key").getValue(), 1);
+    expect(ir.get<IY>(key: "key").getXValue(), 1);
+    expect(ir.get<IY>(key: "key").getValue(), 2);
+    expect(ir.get<IX>(key: "key").getValue(), 1);
+    expect(() => ir.get<IZ>(key: "key"), throwsAssertionError);
+    expect(() => ir.get<IX>(key: "unknown_key"), throwsAssertionError);
+    expect(() => ir.get<IY>(key: "unknown_key"), throwsAssertionError);
+  });
+
+  test('child with params', () {
+    final Injector ic = Injector.root().child();
+    int x = 0;
+    int y = 0;
+    ic.map<IX>((i) => X(++x), key: "key");
+    ic.map<IY>((i) => Y(i.get<IX>(key: "key"), ++y), isSingleton: false, key: "key");
+    expect(ic.get<IX>(key: "key").getValue(), 1);
+    expect(ic.get<IY>(key: "key").getXValue(), 1);
+    expect(ic.get<IY>(key: "key").getValue(), 2);
+    expect(ic.get<IX>(key: "key").getValue(), 1);
+    expect(() => ic.get<IZ>(key: "key"), throwsAssertionError);
+    expect(() => ic.get<IX>(key: "unknown_key"), throwsAssertionError);
+    expect(() => ic.get<IY>(key: "unknown_key"), throwsAssertionError);
+  });
+
+  test('root & child with params', () {
+    final Injector ir = Injector.root();
+    ir.map<IX>((i) => X(100), key: "key");
+    ir.map<IY>((i) => Y(i.get<IX>(key: "key"), 100), isSingleton: false, key: "key");
+    final Injector ic = Injector.root().child();
+    ic.map<IX>((i) => X(200), key: "key");
+    expect(ic.get<IX>(key: "key").getValue(), 200);
+    expect(ic.get<IY>(key: "key").getXValue(), 200);
+    expect(ic.get<IY>(key: "key").getValue(), 100);
+    expect(ic.get<IX>(key: "key").getValue(), 200);
+    expect(ir.get<IX>(key: "key").getValue(), 100);
+    expect(() => ic.get<IX>(key: "unknown_key"), throwsAssertionError);
+    expect(() => ic.get<IY>(key: "unknown_key"), throwsAssertionError);
+  });
+}
